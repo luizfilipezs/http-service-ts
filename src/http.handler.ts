696e002bfd85ec64719ab5bd9a60c1a15d0c88b7
Luiz Filipe da Silva <luizfilipe.tj@icloud.com>
2020-05-11 11:36:08 -0300

    Fix errors in HttpHandler class and add IDE comments

--- src/http.handler.ts
+++ src/http.handler.ts
@@ -1,58 +1,109 @@
-// Informations about the request
-
+/**
+ * Allows `HttpHandler.request()` method to know what to request
+ * and how to do it.
+ */
 interface RequestArgs<T> {
   url?: string;
   method: 'get' | 'post' | 'put' | 'delete';
   headers?: Headers;
   obj?: T;
   id?: number | string;
+  cors?: boolean;
 }
 
-// Request configuration
-
+/**
+ * Define basic configuration for future requests, as headers
+ * and a boolean that allow `request()` method to know if it is necessary
+ * to append a slash to the final of the URL.
+ */
 interface HttpConfig {
   headers: Headers;
+  /**
+   * If `true` a slash will always be appended to the end of the URL (when there is no one)
+   */
+  appendSlash: boolean;
 }
 
-// HTTP requests handler
+/**
+ * Define a type for response content.
+ */
+type responseType = 'json' | 'blob' | 'text';
 
+/**
+ * @class
+ *
+ * Allows to make requests and save some configurations for future
+ * requests too. Accepts an API root at the constructor that can be used
+ * to fix a URL to fetch. If one be provided every future request will
+ * fetch this URL + the URL passed in `RequestArgs` interface.
+ */
 export class HttpHandler {
-  config: HttpConfig;
+  public config: HttpConfig;
 
+  /**
+   * @param {string} [root] You can provide a value for root property
+   * or simply pass one every time you access `request()` method.
+   */
   constructor(private root?: string) {
     this.config = {
       headers: new Headers(),
+      appendSlash: false,
     };
   }
 
-  /*
-    You can provide a value for root property
-    or simply pass one every time you access request() method.
-  */
-
-  async request<T>(args: RequestArgs<T>): Promise<T> {
+  /**
+   * @param {RequestArgs<T>} [args] Provide request configurations
+   * @param {responseType} [format] Define a type for response (e.g. `'json'`)
+   *
+   * @return {Promise<T>} Promise of type T
+   */
+  public async request<T>(args: RequestArgs<T>, format?: responseType): Promise<T> {
     let url = '';
 
-    if (this.root) url = this.root;
-    if (args.url) url.concat(this.hasSlash(url, args.url) ? args.url : `/${args.url}`);
-    if (args.id) url.concat(this.hasSlash(url) ? args.id.toString() : `/${args.id.toString()}`);
+    if (this.root) url = this.root; // Add root if there's one
+    if (this.root && args.url) url += this.hasSlash(this.root, args.url) ? args.url : `/${args.url}`; // Concat root with URI if they were provided
+    if (!this.root && args.url) url = args.url; // fetch URL provided in arguments
+
+    if (this.config.appendSlash && !this.hasSlash(url)) url += '/';
 
     const requestInit: RequestInit = {
       method: args.method,
       headers: args.headers || this.config.headers,
+      mode: args.cors === false ? 'no-cors' : 'cors',
     };
-
     if (args.method !== 'get' && args.obj) requestInit.body = JSON.stringify(args.obj);
 
-    return await fetch(url, requestInit).then(this.json);
+    return await fetch(url, requestInit).then((response) => this.parse<T>(response, format) as Promise<T>);
   }
 
-  // Avoids errors concatening the URLs
-
+  /**
+   * @param {string} [start] First piece of URL (API root). E.g. `'https://api.example.com'`
+   * @param {string} [final] Last piece of URL. E.g. `'users/12'`
+   *
+   * @return {string} URL with a slash between its first and last pieces or a slash at the final of the first.
+   */
   private hasSlash = (start: string, final?: string) =>
     final ? start.endsWith('/') || final.startsWith('/') : start.endsWith('/');
 
-  // Turn response to JSON
+  /**
+   * @param {Response} [response] Get a response to turn into JSON, Text or Blob
+   * @param {responseType} [format] Get the Content-Type that must be returned
+   *
+   * @return {Promise<T | string | {} | Blob>} New promise with the content type defined in `format` param
+   */
+  private parse<T>(response: Response, format: responseType = 'json'): Promise<T | string | {} | Blob> {
+    const contentType = response.headers.get('content-type');
 
-  private json = (response: Response) => (response.headers.has('Content-Type') ? response.json() : response);
+    if (contentType === 'application/json')
+      // JSON
+      return response.json();
+
+    if (contentType && contentType.startsWith('text'))
+      // TEXT
+      return response.text();
+
+    if (!contentType) return new Promise((resolve, reject?) => resolve({}));
+
+    return response.blob();
+  }
 }
--- src/service.ts
+++ src/service.ts
@@ -1,28 +1,56 @@
 import { HttpHandler } from './http.handler';
 
-// Service with the main HTTP protocols as methods, expecting a type (T) for the returns
-
+/**
+ * @class
+ *
+ * Customizable service that allow to perform main HTTP requests.
+ * Extends `HttpHandler` class. So it's possible to add more types of
+ * request when extending this class.
+ */
 export class Service<T> extends HttpHandler {
+  /**
+   * @param {string} [apiRoot] Get the path of a collection (e.g. `"https://api.example.com/users/"`)
+   */
   constructor(apiRoot: string) {
     super(apiRoot);
   }
 
+  /**
+   * @return {Promise<T | T[]>} An object promise or array of objects promise
+   */
   get(): Promise<T | T[]> {
     return this.request<T | T[]>({ method: 'get' });
   }
 
+  /**
+   * @param {number} [id] Object ID to fetch
+   * @return {Promise<T>} A promise of object
+   */
   getById(id: number): Promise<T> {
     return this.request<T>({ method: 'get', id });
   }
 
+  /**
+   * @param {T} [obj] Object to post
+   * @return {Promise<T>} Object posted
+   */
   post(obj: T): Promise<T> {
     return this.request<T>({ method: 'post', obj });
   }
 
+  /**
+   * @param {T} [obj] Object to update
+   * @param {number} [id] ID of object that will be updated
+   * @return {Promise<T>} Object updated
+   */
   put(obj: T, id: number): Promise<T> {
     return this.request<T>({ method: 'put', obj, id });
   }
 
+  /**
+   * @param {number} [id] ID of object that will be deleted
+   * @return {Promise<{}>} Empty object
+   */
   delete(id: number): Promise<{}> {
     return this.request<{}>({ method: 'delete', id });
   }
